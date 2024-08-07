bb55c4c439e4375dcd933f8333449ae410f62b07
Ho Chun Wong <sc23hcw@leeds.ac.uk>
2024-08-08 06:13:19 +0800

    trackFinder9000 finally done

--- trackFinder.js
+++ trackFinder.js
@@ -1,19 +1,39 @@
 // Geolocation script to find nearby tracks
 
 function success(pos) {
+
+    // Get user's latitude & longitude
+
     console.log(pos);
     let lat = pos.coords.latitude;
     let lng = pos.coords.longitude;
+    let loc = "";
     const pleaseNoSteal = "QUl6YVN5QzN2U2NyVEJzSXhjUXVIWTduTjA1VmFBdVNtS2F4MTM4";
 
-    const markup = `
-    <p>Showing go-kart tracks near ${lat} ${lng}:</p>
-    <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/search?q=go-kart%20tracks%20near%20${lat}%20${lng}&key=${atob(pleaseNoSteal)}"></iframe> 
+
+    // Reverse Geocode to get City
+
+    const reverseGeocode = `
+    https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${atob(pleaseNoSteal)}
     `;
+    fetch(reverseGeocode)
+    .then(response => response.json() )
+    .then(data => {
+        let loc = data.plus_code.compound_code;
+        console.log(loc);
 
-    console.log(markup);
+        // Find nearby kart tracks in City
 
-    document.getElementById("map").innerHTML = markup;
+        const map = `
+        <p>Showing go-kart tracks near ${lat} ${lng} (${loc}):</p>
+        <iframe width="100%" height="720" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/search?q=go-kart%20tracks%20near%20${loc}&key=${atob(pleaseNoSteal)}"></iframe> 
+        `;
+
+        console.log(map);
+
+        document.getElementById("map").innerHTML = map;
+    })
+    .catch(err => console.warn(err.message));
 }
 
 
